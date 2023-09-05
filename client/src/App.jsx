b89235b4d67e119ad86e36b021eb2f64a2d10e0b
sealproton <sealproton@gmail.com>
2023-09-05 12:12:14 +0700

    feat: setDefaultOption for queryClient

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,39 @@
-import "./App.css";
-import { ChakraProvider } from "@chakra-ui/react";
-import React from "react";
+import './App.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import AuthenticatedApp from './pages/AuthenticatedApp.jsx';
+import UnauthenticatedApp from './pages/UnauthenticatedApp.jsx';
+import { useAuth } from './contexts/authentication';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
-import AuthenticatedApp from "./pages/AuthenticatedApp.jsx";
-import UnauthenticatedApp from "./pages/UnauthenticatedApp.jsx";
-import { useAuth } from "./contexts/authentication";
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+const queryClient = new QueryClient();
 
-const queryClient = new QueryClient()
+const getToken = () => {
+  return window.localStorage.getItem('token');
+};
+queryClient.setDefaultOptions({
+  queries: {
+    defaultOptions: {
+      headers: {
+        Authorization: `Bearer ${getToken()}`,
+      },
+    },
+    onError: (error) => {
+      if (
+        error.response.status === 401 &&
+        error.response.statusText === 'Unauthorized'
+      ) {
+        window.alert(error.response.data.message);
+        window.localStorage.removeItem('token');
+        window.location.replace('/login');
+      }
+      if (error.response.status === 404) {
+        window.alert(error.response.data.message);
+        window.location.replace('/login');
+      }
+      return Promise.reject(error);
+    },
+  },
+});
 
 function App() {
   const auth = useAuth();

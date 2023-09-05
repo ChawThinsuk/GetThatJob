91eedece917172c85473e77cb572b2adf9ee1baa
sealproton <sealproton@gmail.com>
2023-09-05 14:33:01 +0700

    feat: add JWT interceptor and delete setDefaultOption

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,43 +7,15 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 
 const queryClient = new QueryClient();
 
-const getToken = () => {
-  return window.localStorage.getItem('token');
-};
-queryClient.setDefaultOptions({
-  queries: {
-    defaultOptions: {
-      headers: {
-        Authorization: `Bearer ${getToken()}`,
-      },
-    },
-    onError: (error) => {
-      if (
-        error.response.status === 401 &&
-        error.response.statusText === 'Unauthorized'
-      ) {
-        window.alert(error.response.data.message);
-        window.localStorage.removeItem('token');
-        window.location.replace('/login');
-      }
-      if (error.response.status === 404) {
-        window.alert(error.response.data.message);
-        window.location.replace('/login');
-      }
-      return Promise.reject(error);
-    },
-  },
-});
-
 function App() {
   const auth = useAuth();
   return (
-  <ChakraProvider>
-    {/* <QueryClientProvider client={queryClient}> */}
-      {auth.isAuthenticated ? <AuthenticatedApp /> : <UnauthenticatedApp />}
-    {/* </QueryClientProvider> */}
-  </ChakraProvider>
-  )
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        {auth.isAuthenticated ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      </QueryClientProvider>
+    </ChakraProvider>
+  );
 }
 
 export default App;

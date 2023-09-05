852d1be0deda7c84249c5f35a373b0f91d935de1
sealproton <sealproton@gmail.com>
2023-09-05 14:33:01 +0700

    feat: add JWT interceptor and delete setDefaultOption

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import registerRouter from "./Router/RegisterRouter.js";
-import authRouter from "./Router/AuthRouter.js"
-
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import registerRouter from './Router/RegisterRouter.js';
+import authRouter from './Router/AuthRouter.js';
+import { protect } from './middlewares/protect.js';
 async function init() {
   const app = express();
   const PORT = process.env.PORT || 4000;
@@ -11,10 +11,10 @@ async function init() {
   app.use(bodyParser.json());
   app.use(cors());
   app.use(express.json());
-  app.use("/register", registerRouter);
-  app.use("/auth", authRouter)
+  app.use('/register', registerRouter);
+  app.use('/auth', authRouter);
 
-  app.get('/', (req, res) => {
+  app.get('/', [protect], (req, res) => {
     return res.json({
       message: 'Hello',
     });

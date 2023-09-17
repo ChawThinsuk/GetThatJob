aa2044a0ea9fbe19b68bc6ade260560bc9b90f12
mrakkapol <mr.akkapol@gmail.com>
2023-09-17 18:55:33 +0700

    feat : add update recruiter profile

--- client/src/components/Recruiter/RecruiterProfile.jsx
+++ client/src/components/Recruiter/RecruiterProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   ChakraProvider,
   Box,
@@ -12,41 +12,179 @@ import {
   Input,
   Stack,
   Textarea,
+  useToast,
 } from "@chakra-ui/react";
 import { useGlobalContext } from "../../contexts/registerContext.jsx";
+import axios from "axios";
 import UploadPdf from "../register/UploadPdf.jsx";
+import { useAuth } from "../../contexts/Authorization.jsx";
+import uploadlogo from "../../assets/register-images/pdf-upload.svg";
 
 export function RecruiterProfile() {
-  const {
-    profFormStyle,
-    companyName,
-    setCompanyName,
-    recruiterEmail,
-    setRecruiterEmail,
-    recruiterPassword,
-    setRecruiterPassword,
-    recruiterpasswordConfirmation,
-    setRecruiterPasswordConfirmation,
-    companyWebsite,
-    setCompanyWebsite,
-    aboutCompany,
-    setAboutCompany,
-  } = useGlobalContext();
+  const { profFormStyle, userType } = useGlobalContext();
+  const [companyName, setCompanyName] = useState("");
+  const [recruiterEmail, setRecruiterEmail] = useState("");
+  const [recruiterPassword, setRecruiterPassword] = useState("");
+  const [recruiterpasswordConfirmation, setRecruiterPasswordConfirmation] =
+    useState("");
+  const [companyWebsite, setCompanyWebsite] = useState("");
+  const [aboutCompany, setAboutCompany] = useState("");
+  const [logo, setLogo] = useState("");
+  const [selectedFileName, setSelectedFileName] = useState(null);
+  const [formattedUpdatedTime, setFormattedUpdatedTime] = useState("");
+  const { state } = useAuth();
+  const toast = useToast();
 
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      // Check for allowed file types (JPG and PNG)
+      if (/(jpg|jpeg|png)$/i.test(file.type) && file.size <= 5 * 1024 * 1024) {
+        setLogo(file);
+        setSelectedFileName(file.name);
+      } else {
+        setLogo(null);
+        setSelectedFileName(null);
+        toast({
+          title: "Wrong file type or size",
+          description: "Please upload a JPG or PNG file under 5MB.",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      }
+    } else {
+      // No file selected, clear the selected file and file name
+      setLogo(null);
+      setSelectedFileName(null);
+    }
+  };
+
+  const getRecfProfile = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/aoo/getrecruiter",
+        { id: state.userID }
+      );
+
+      const updatedTime = response.data.data.updated_at;
+      const date = new Date(updatedTime);
+      const newFormattedUpdatedTime = `${date
+        .toISOString()
+        .slice(0, 10)} at ${date.toLocaleTimeString()}`;
+
+      const { data } = response;
+
+      const {
+        recruiter_id,
+        company_name,
+        company_email,
+        company_description,
+        company_website,
+        logo,
+        user_id,
+      } = data.data;
+
+      setCompanyName(data.data.company_name);
+      setAboutCompany(data.data.company_description);
+      setRecruiterEmail(data.data.company_email);
+      setCompanyWebsite(data.data.company_website);
+      setLogo(data.data.logo);
+      setSelectedFileName(data.data.logo);
+      setFormattedUpdatedTime(newFormattedUpdatedTime);
+      // console.log(logo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSaveChanges = async () => {
+    try {
+      // Prepare the updated profile data object
+      const updatedRecData = {
+        company_email: recruiterEmail,
+        company_name: companyName,
+        company_website: companyWebsite,
+        company_description: aboutCompany,
+      };
+
+      // Make a PUT request to update the profile data
+      await axios.put(
+        `http://localhost:4000/aoo/getrecruiter/${state.userID}`,
+        updatedRecData
+      );
+
+      // Display a success message to the user
+      toast({
+        title: "Profile updated successfully",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+    } catch (error) {
+      // Handle any errors that may occur during the update process
+      console.error("Error updating profile:", error);
+      toast({
+        title: "Error updating profile",
+        description:
+          "An error occurred while updating your profile. Please try again later.",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  };
+
+  useEffect(() => {
+    getRecfProfile();
+  }, []);
   return (
     <ChakraProvider>
       <div className="flex pl-[160px] font-[Inter]">
         <Box w="100%" maxW="lg" mt={10} borderRadius="md">
           <h1 className="text-[45px] font-[Montserrat] mb-4">Profile</h1>
           <div className="flex mb-[11px]">
-            <div className="w-[100px] h-[100px] bg-fuchsia-700 rounded-2xl shadow-lg">
-              Logo
+            <div className="w-[100px] h-[100px]  rounded-2xl shadow-lg">
+              <img src={logo} alt="" />
             </div>
             <div className="ml-2 flex flex-col">
               <p className="mb-3 text-[#373737] text-[13px] tracking-[1.5px] uppercase">
                 company logo
               </p>
-              <UploadPdf />
+              {/* <UploadPdf /> */}
+              <div>
+                <div className="mx-auto bg-white rounded-lg flex">
+                  <input
+                    type="file"
+                    id="pdf-upload"
+                    className="hidden"
+                    onChange={handleFileChange}
+                    accept=".jpg,.jpeg,.png"
+                  />
+                  <label
+                    htmlFor="pdf-upload"
+                    className="cursor-pointer flex items-center justify-center w-[160px] h-auto p-[13px] rounded-xl bg-[#F48FB1] text-white hover:bg-pink-600 transition duration-300"
+                  >
+                    <img src={uploadlogo} className="pr-2" alt="logo" />
+                    Choose a file
+                  </label>
+
+                  {selectedFileName ? (
+                    <div className="mt-4 ml-4">
+                      <p>
+                        File selected:{" "}
+                        {selectedFileName.substring(
+                          selectedFileName.length - 12
+                        )}
+                      </p>
+                    </div>
+                  ) : (
+                    <div className="ml-4 mt-3">
+                      <p>No file chosen</p>
+                    </div>
+                  )}
+                </div>
+              </div>
               <p className="mt-2 text-[#8E8E8E]">
                 Only JPG,JPEG,PNG. Max size 5MB
               </p>
@@ -131,19 +269,23 @@ export function RecruiterProfile() {
                 />
               </FormControl>
             </Stack>
+            <p className="mt-2 text-[#8E8E8E] text-[16px]">
+              Last Updated: {formattedUpdatedTime}
+            </p>
             <Button
               letterSpacing="2px"
               w="220px"
               h="53px"
               mt={8}
               mb={8}
-              type="submit"
+              type="button"
               bg="#F48FB1"
               variant="solid"
               size="sm"
               fontSize="19px"
               color="white"
               borderRadius="19px"
+              onClick={handleSaveChanges}
             >
               UPDATE PROFILE
             </Button>
--- server/Router/AooRouter.js
+++ server/Router/AooRouter.js
@@ -23,6 +23,42 @@ AooRouter.get("/:id", async (req, res) => {
   }
 });
 
+AooRouter.post("/getrecruiter", async (req, res) => {
+  const recruiter_id = req.body.id;
+  // console.log(recruiter_id);
+
+  if (!recruiter_id) {
+    return res.status(400).json({
+      message: "Recruiter ID is required",
+    });
+  }
+
+  try {
+    const result = await pool.query(
+      "SELECT * FROM recruiters WHERE user_id = $1",
+      [recruiter_id]
+    );
+    // console.log(result);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        message: "Recruiter not found",
+      });
+    }
+
+    return res.json({
+      data: result.rows[0],
+      message: "Get recruiter's profile successfully",
+    });
+  } catch (error) {
+    console.error("Error:", error.stack);
+    return res.status(500).json({
+      message: "An error occurred",
+      error: error.message,
+    });
+  }
+});
+
 AooRouter.post("/:id", async (req, res) => {
   const user_id = req.params.id;
   // const {
@@ -60,7 +96,8 @@ AooRouter.post("/:id", async (req, res) => {
         title = $6,
         experience = $7,
         education = $8,
-        cv = $9
+        cv = $9,
+       
       WHERE user_id = $1;
       `,
       [
@@ -95,6 +132,49 @@ AooRouter.post("/:id", async (req, res) => {
       message:
         "An error occurred while updating your profile. Please try again later.",
       error: error.message, // Include the error message in the response
+    });
+  }
+});
+
+AooRouter.put("/getrecruiter/:id", async (req, res) => {
+  const user_id = req.params.id;
+
+  const company_email = req.body.company_email;
+  const company_name = req.body.company_name;
+  const company_website = req.body.company_website;
+  const company_description = req.body.company_description;
+
+  try {
+    // Update the recruiter's profile data in the database
+    await pool.query(
+      `
+      UPDATE recruiters
+      SET
+        company_email = $2,
+        company_name = $3,
+        company_website = $4,
+        company_description = $5,
+        updated_at = NOW()
+      WHERE user_id = $1;
+      `,
+      [
+        user_id,
+        company_email,
+        company_name,
+        company_website,
+        company_description,
+      ]
+    );
+
+    return res.json({
+      message: "Recruiter profile updated successfully",
+    });
+  } catch (error) {
+    console.error("Error updating profile:", error);
+    return res.status(500).json({
+      message:
+        "An error occurred while updating your profile. Please try again later.",
+      error: error.message,
     });
   }
 });

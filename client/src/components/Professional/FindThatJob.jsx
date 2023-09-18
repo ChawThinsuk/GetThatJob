e8bea0366b647f2917724f2f53560e47dd62206d
Siniiez <siniiez@Siniiezs-MacBook-Air.local>
2023-09-18 13:30:27 +0700

    fix: edit size findThatJobPage

--- client/src/components/Professional/FindThatJob.jsx
+++ client/src/components/Professional/FindThatJob.jsx
@@ -19,18 +19,18 @@ export const FindThatJob = () => {
   return (
     <div className="flex flex-col justify-start items-center w-full min-h-srceen pr-[100px] pl-[100px] pt-[50px] font-[Inter] bg-[#F5F5F6]">
       <div className="flex flex-col justify-center items-start w-full">
-        <p className="text-[34px] text-start w-full font-[Montserrat]">
+        <p className="text-[46px] text-start w-full font-[Montserrat]">
           Find That Job
         </p>
-        <p className="text-[10px] pt-[16px]">
+        <p className="text-[14px] pt-[16px]">
           SEARCH BY JOB TITLE OR COMPANY NAME
         </p>
-        <div className="border-[1px] border-[#F48FB1] rounded-[8px] w-[420px] h-[36px] flex flex-row justify-start items-center bg-[#FFFFFF]">
-          <img src={find} className="w-[16.93] h-[16.93] pl-2" />
+        <div className="border-[1px] border-[#F48FB1] rounded-[8px] w-[560px] h-[42px] flex flex-row justify-start items-center bg-[#FFFFFF]">
+          <img src={find} className="w-[22] h-[22] pl-2" />
           <input
             type="text"
             placeholder="manufacturing, sales, swim"
-            className="w-[376px] h-[20px] text-[14px] p-[8px] leading-6 outline-none"
+            className="w-[500px] h-[27px] text-[18px] p-[8px] leading-6 outline-none"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
@@ -38,11 +38,11 @@ export const FindThatJob = () => {
         <div className="flex flex-rows pt-[8px]">
           <div className="pr-[16px]">
             <label>
-              <p className="text-[10px]">CATEGORY</p>
+              <p className="text-[14px]">CATEGORY</p>
               <select
                 value={category}
                 onChange={(e) => setCategory(e.target.value)}
-                className="border-[1px] border-[#F48FB1] rounded-[8px] w-[280px] h-[36px] flex flex-col justify-center text-[14px] p-[8px] text-[#8E8E8E]"
+                className="border-[1px] border-[#F48FB1] rounded-[8px] w-[375px] h-[42px] flex flex-row justify-center items-center text-[18px] p-[8px] text-[#8E8E8E]"
               >
                 <option value={""}>Select a category</option>
                 <option>Software Developer</option>
@@ -54,11 +54,11 @@ export const FindThatJob = () => {
           </div>
           <div className="pr-[16px]">
             <label>
-              <p className="text-[10px]">TYPE</p>
+              <p className="text-[14px]">TYPE</p>
               <select
                 value={type}
                 onChange={(e) => setType(e.target.value)}
-                className="border-[1px] border-[#F48FB1] rounded-[8px] w-[280px] h-[36px] flex flex-col justify-center text-[14px] p-[8px] text-[#8E8E8E]"
+                className="border-[1px] border-[#F48FB1] rounded-[8px] w-[375px] h-[42px] flex flex-row justify-center items-center text-[18px] p-[8px] text-[#8E8E8E]"
               >
                 <option value={""}>Select a type</option>
                 <option>Full time</option>
@@ -67,24 +67,24 @@ export const FindThatJob = () => {
             </label>
           </div>
           <div>
-            <p className="text-[10px]">SALARY RANGE</p>
-            <div className="flex flex-rows justify-between items-center w-[231px]">
-              <div className="border-[1px] border-[#F48FB1] rounded-[8px] w-[102px] h-[36px] flex flex-row justify-start items-center bg-[#FFFFFF]">
+            <p className="text-[14px]">SALARY RANGE</p>
+            <div className="flex flex-rows justify-between items-center w-[308px]">
+              <div className="border-[1px] border-[#F48FB1] rounded-[8px] w-[136px] h-[42px] flex flex-row justify-start items-center bg-[#FFFFFF]">
                 <img src={money} className="pl-2" />
                 <input
                   type="text"
                   placeholder="min"
-                  className="w-[58px] h-[20px] flex flex-col justify-center text-[14px] p-[8px] leading-6 outline-none"
+                  className="w-[77px] h-[27px] flex flex-col justify-center text-[18px] p-[8px] leading-6 outline-none"
                   onChange={(e) => setMinSalary(e.target.value)}
                 />
               </div>
               <p>-</p>
-              <div className="border-[1px] border-[#F48FB1] rounded-[8px] w-[102px] h-[36px] flex flex-row justify-start items-center bg-[#FFFFFF]">
+              <div className="border-[1px] border-[#F48FB1] rounded-[8px] w-[136px] h-[42px] flex flex-row justify-start items-center bg-[#FFFFFF]">
                 <img src={money} className="pl-2" />
                 <input
                   type="text"
                   placeholder="max"
-                  className="w-[58px] h-[20px] flex flex-col justify-center text-[14px] p-[8px] leading-6 outline-none"
+                  className="w-[77px] h-[27px] flex flex-col justify-center text-[18px] p-[8px] leading-6 outline-none"
                   onChange={(e) => setMaxSalary(e.target.value)}
                 />
               </div>
@@ -98,7 +98,7 @@ export const FindThatJob = () => {
             {jobs.length} jobs for you
           </p>
         </div>
-        <div className="flex flex-wrap justify-start gap-[16px] w-[960px] h-srceen">
+        <div className="flex flex-wrap justify-start gap-[16px] w-[1280px] h-srceen">
           {jobs.map((job) => (
             <JobCard job={job} key={job.job_id} />
           ))}
--- client/src/components/Professional/JobCard.jsx
+++ client/src/components/Professional/JobCard.jsx
@@ -53,41 +53,41 @@ const JobCard = (prop) => {
   return (
     <div
       key={job.job_id}
-      className="w-[290px] h-[170px] rounded-[8px] border-[1px] border-[#E1E2E1] bg-[#FFFFFF] flex justify-center items-center mr-[16px] shadow-pro1"
+      className="w-[390px] h-[227px] rounded-[8px] border-[1px] border-[#E1E2E1] bg-[#FFFFFF] flex justify-center items-center mr-[16px] shadow-pro1"
     >
-      <div className="w-[258px] h-[138px] flex flex-col justify-between items-center">
-        <div className="flex flex-row w-[258px] h-[82px] gap-[8px]">
-          <img src={job.logo} className="w-[80px] h-[80px]" />
-          <div className="flex flex-col w-[170px] h-[82px]">
-            <div className="flex flex-row gap-[4px]">
-              <img src={manufacturing} className="w-[15px] h-[15px]" />
-              <p className="text-[12px] text-[#8E8E8E] min-w-[106px] h-[16px]">
+      <div className="w-[345px] h-[185px] flex flex-col justify-between items-center">
+        <div className="flex flex-row w-[345px] h-[110px] gap-[10px]">
+          <img src={job.logo} className="w-[100px] h-[100px]" />
+          <div className="flex flex-col w-[230px] h-[110px]">
+            <div className="flex flex-row gap-[5px]">
+              <img src={manufacturing} className="w-[20px] h-[20px]" />
+              <p className="text-[16px] text-[#8E8E8E] min-w-[142px] h-[22px]">
                 {job.job_category}
               </p>
             </div>
-            <div className="flex flex-col w-[170px] h-[48px]">
-              <p className="text-[20px] text-[#373737] min-w-[200px] h-[28px]">
+            <div className="flex flex-col w-[230px] h-[64px]">
+              <p className="text-[24px] text-[#373737] w-[230px] h-[38px]">
                 {job.job_title}
               </p>
-              <p className="text-[14px] text-[#616161] w-[170px] h-[18px]">
+              <p className="text-[18px] text-[#616161] w-[230px] h-[24px]">
                 {job.company_name}
               </p>
             </div>
-            <div className="flex flex-row justify-between items-center w-[170px] h-[20px] pt-[4px]">
-              <div className="flex flex-row justify-center items-center gap-[4px] w-[80px] h-[20px] ">
-                <img src={calendar} className="w-[15px] h-[16px]" />
-                <p className="text-[12px] text-[#8E8E8E] w-[70px] h-[16px]">
+            <div className="flex flex-row justify-between items-center w-[230px] h-[27px] pt-[4px]">
+              <div className="flex flex-row justify-center items-center gap-[5px] w-[106px] h-[27px] ">
+                <img src={calendar} className="w-[20-px] h-[20px]" />
+                <p className="text-[16px] text-[#8E8E8E] w-[95px] h-[22px]">
                   {job.job_type}
                 </p>
               </div>
-              <div className="flex flex-row justify-center items-center min-w-[84px] h-[20px] ">
-                <img src={small} className="w-[15px] h-[16px]" />
-                <div className="flex flex-row justify-center items-center min-w-[65px] h-[16px]">
-                  <p className="text-[12px] text-[#8E8E8E]">
+              <div className="flex flex-row justify-center items-center min-w-[112px] h-[27px] ">
+                <img src={small} className="w-[20px] h-[20px]" />
+                <div className="flex flex-row justify-center items-center min-w-[86px] h-[22px]">
+                  <p className="text-[16px] text-[#8E8E8E]">
                     {(job.salary_min / 1000).toFixed(1)}k
                   </p>
                   <p>-</p>
-                  <p className="text-[12px] text-[#8E8E8E]">
+                  <p className="text-[16px] text-[#8E8E8E]">
                     {(job.salary_max / 1000).toFixed(1)}k
                   </p>
                 </div>
@@ -95,23 +95,23 @@ const JobCard = (prop) => {
             </div>
           </div>
         </div>
-        <div className="flex flex-row justify-between w-[258px] h-[40px] pt-[4px]">
+        <div className="flex flex-row justify-between w-[345px] h-[54px] pt-[5px]">
           <div
-            className="w-[109px] h-[40px] flex flex-row justify-center items-center"
+            className="w-[147px] h-[54px] flex flex-row justify-center items-center"
             onClick={handleChangeStatus}
           >
-            <div className="w-[40px] h-[40px] flex flex-row justify-center items-center">
+            <div className="w-[54px] h-[53px] flex flex-row justify-center items-center">
               <img
                 src={follow && follow.job_user_following ? followOn : followOff}
-                className="w-[24px] h-[24px]"
+                className="w-[32px] h-[32px]"
               />
             </div>
-            <button className=" text-[14px] text-[#616161] font-[Inter] font-[500] tracking-[1.25px] w-[69px] h-[40px] flex flex-row justify-center items-center">
+            <button className=" text-[18px] text-[#616161] font-[Inter] font-[500] tracking-[1.25px] w-[92px] h-[54px] flex flex-row justify-center items-center">
               {follow && follow.job_user_following ? " Following" : "Follow"}
             </button>
           </div>
           <Link to={`/${job.job_id}`}>
-            <button className="w-[109px] h-[40px] border-[1px] border-[#F48FB1] rounded-xl hover:bg-[#F5F5F6] text-[14px] text-[#616161] font-[Inter] font-[500]">
+            <button className="w-[147px] h-[54px] border-[1px] border-[#F48FB1] rounded-xl hover:bg-[#F5F5F6] text-[18px] text-[#616161] font-[Inter] font-[500]">
               SEE MORE
             </button>
           </Link>

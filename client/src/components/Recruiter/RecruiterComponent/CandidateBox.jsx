0ef0de8d6f26d26a77896a6058760b442b024db8
chawthinsuk <chawbanthin@gmail.com>
2023-09-23 23:41:50 +0700

    fix: api camdidate

--- client/src/components/Recruiter/RecruiterComponent/CandidateBox.jsx
+++ client/src/components/Recruiter/RecruiterComponent/CandidateBox.jsx
@@ -14,11 +14,12 @@ import {
 function Candidate(props) {
   const data = props.datas;
   const [item, setItemm] = useState(props.datas);
-  const date = calculateDayAgo(data.created_at);
+  const date = calculateDayAgo(data.application_created_at);
   const handleToggleActive = () => {
     let newActive = item.active === 1 ? 0 : 1;
     setItemm({ ...item, active: newActive });
   };
+  console.log(data.application_created_at)
   return (
     <div
       className={`flex flex-col justify-between  w-[1258.72px] rounded-[8px] mt-[21.33px] shadow-md group ${
@@ -114,7 +115,7 @@ function Candidate(props) {
             ) : (
               <p>
                 sent on
-                <br /> {formatDate(data.created_at)}
+                <br /> {formatDate(data.application_created_at)}
               </p>
             )}
           </div>

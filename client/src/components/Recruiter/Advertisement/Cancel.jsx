43ce4699c8b4086011dfd1e61b74f7960e2ed8ab
sealproton <sealproton@gmail.com>
2023-09-27 16:46:31 +0700

    feat: set axios for success page

--- client/src/components/Recruiter/Advertisement/Cancel.jsx
+++ client/src/components/Recruiter/Advertisement/Cancel.jsx
@@ -1,40 +1,75 @@
 import { useEffect, useState } from 'react';
-import { useQuery, useMutation } from 'react-query';
+import { Spinner } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import { BsFillExclamationCircleFill } from 'react-icons/bs';
+import axios from 'axios';
 
 function Cancel() {
+  const [isLoading, setIsloading] = useState(false);
+  const getSession = async () => {
+    setIsloading(true);
+    try {
+      const urlParams = new URLSearchParams(window.location.search).get(
+        'job_id'
+      );
+      const paymentStatus = await axios(
+        `http://localhost:4000/ads/success/jobs/${urlParams}`
+      );
+      setIsloading(false);
+    } catch (error) {
+      setIsloading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getSession();
+  }, []);
   return (
     <div className='w-screen h-screen flex flex-col justify-center items-center'>
-      <section className='flex flex-col items-center w-[40%]  bg-gray-100 rounded-lg border-[1px] border-gray-200 shadow-lg p-8'>
-        <BsFillExclamationCircleFill className='w-[100px] h-[100px] text-amber-400' />
-        <p className='mt-8 text-[40px] font-[Inter] font-bold text-gray-600'>
-          Payment Canceled
-        </p>
-
-        <p className='mt-5 font-[Inter] text-[20px] w-full text-center border-b-2 border-gray-300 pb-4'>
-          Your transaction has been canceled.
-        </p>
-        <div className='flex gap-5'>
-          <Link to='/'>
-            <button
-              type='button'
-              className='mt-5 bg-[#f6a6c1] hover:bg-[#fa93b5] rounded-xl w-[200px] h-[50px] font-[Inter] text-[18px] font-semibold text-white'
-            >
-              Back to home page
-            </button>
-          </Link>
-          <Link to='/ads'>
-            <button
-              type='button'
-              className='mt-5 bg-[#f6a6c1] hover:bg-[#fa93b5] rounded-xl min-w-[200px] pr-2 pl-2 h-[50px] font-[Inter] text-[18px] font-semibold text-white'
-            >
-              Back to Advertisement page
-            </button>
-          </Link>
+      {isLoading ? (
+        <div className='w-full h-fill flex justify-center items-center'>
+          <Spinner
+            thickness='4px'
+            speed='0.65s'
+            emptyColor='gray.200'
+            color='#F48FB1'
+            size='xl'
+          />
         </div>
-      </section>
+      ) : (
+        <>
+          <section className='flex flex-col items-center w-[40%]  bg-gray-100 rounded-lg border-[1px] border-gray-200 shadow-lg p-8'>
+            <BsFillExclamationCircleFill className='w-[100px] h-[100px] text-amber-400' />
+            <p className='mt-8 text-[40px] font-[Inter] font-bold text-gray-600'>
+              Payment Canceled
+            </p>
+
+            <p className='mt-5 font-[Inter] text-[20px] w-full text-center border-b-2 border-gray-300 pb-4'>
+              Your transaction has been canceled.
+            </p>
+            <div className='flex gap-5'>
+              <Link to='/'>
+                <button
+                  type='button'
+                  className='mt-5 bg-[#f6a6c1] hover:bg-[#fa93b5] rounded-xl w-[200px] h-[50px] font-[Inter] text-[18px] font-semibold text-white'
+                >
+                  Back to home page
+                </button>
+              </Link>
+              <Link to='/ads'>
+                <button
+                  type='button'
+                  className='mt-5 bg-[#f6a6c1] hover:bg-[#fa93b5] rounded-xl min-w-[200px] pr-2 pl-2 h-[50px] font-[Inter] text-[18px] font-semibold text-white'
+                >
+                  Back to Advertisement page
+                </button>
+              </Link>
+            </div>
+          </section>
+        </>
+      )}
     </div>
   );
 }
+
 export default Cancel;
--- server/Router/AdsRouter.js
+++ server/Router/AdsRouter.js
@@ -1,4 +1,4 @@
-import { Router, query } from 'express'; 
+import { Router, query } from 'express';
 import { pool } from '../utils/db.js';
 import { protect } from '../middlewares/protect.js';
 import dotenv from 'dotenv';
@@ -10,47 +10,52 @@ const YOUR_DOMAIN = 'http://localhost:5173';
 const stripeSecretKey = process.env.SECRET_KEY_STRIPE;
 const stripeClient = stripe(stripeSecretKey);
 
-const adsRouter = Router()
+const adsRouter = Router();
 const updateTimestamp = new Date();
+adsRouter.use(protect);
 adsRouter.get('/:user_id', async (req, res) => {
-    const user_id = req.params.user_id
-    const queryParams = [user_id];
-    const query = 'SELECT jobs.job_id, jobs.job_title, jobs.job_category, jobs.salary_min, jobs.salary_max, jobs.job_type, jobs.job_position, jobs.job_mandatory, jobs.job_optional, jobs.created_at, jobs.job_location, recruiters.company_name, recruiters.logo FROM jobs INNER JOIN recruiters ON jobs.recruiter_id = recruiters.recruiter_id WHERE recruiters.user_id = $1';
-    
-    try {
-      const jobs = await pool.query(query, queryParams);
-      console.log(jobs);
-      return res.json({
-        data: jobs,
-      });
-    } catch (error) {
-      return res.status(500).json({ message: error });
-    }
+  const user_id = req.params.user_id;
+  const queryParams = [user_id];
+  const query =
+    'SELECT jobs.job_id, jobs.job_title, jobs.job_category, jobs.salary_min, jobs.salary_max, jobs.job_type, jobs.job_position, jobs.job_mandatory, jobs.job_optional, jobs.created_at, jobs.job_location, recruiters.company_name, recruiters.logo FROM jobs INNER JOIN recruiters ON jobs.recruiter_id = recruiters.recruiter_id WHERE recruiters.user_id = $1';
+
+  try {
+    const jobs = await pool.query(query, queryParams);
+    console.log(jobs);
+    return res.json({
+      data: jobs,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
 });
 
 adsRouter.post('/create-checkout-session/:job_id/:budget', async (req, res) => {
-    const { job_id, budget } = req.params;
-    try {
-      const session = await stripeClient.checkout.sessions.create({
-        line_items: [
-          {
-            price: 'price_1Nu6nKJsRypturIHBgIPSzF6',
-            quantity: Number(budget),
-          },
-        ],
-        mode: 'payment',
-        success_url: `${YOUR_DOMAIN}/ads/success?job_id=${job_id}`,
-        cancel_url: `${YOUR_DOMAIN}/ads/cancle?job_id=${job_id}`,
-      });
-      await pool.query('INSERT INTO transactions (job_id, session_id, created_at, payment_amount) values ($1, $2, $3, $4)',[job_id, session.id, new Date(), budget])
-      res.json({url:session.url});
-    } catch (error) {
-      console.error('Error creating checkout session:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+  const { job_id, budget } = req.params;
+  try {
+    const session = await stripeClient.checkout.sessions.create({
+      line_items: [
+        {
+          price: 'price_1Nu6nKJsRypturIHBgIPSzF6',
+          quantity: Number(budget),
+        },
+      ],
+      mode: 'payment',
+      success_url: `${YOUR_DOMAIN}/ads/success?job_id=${job_id}`,
+      cancel_url: `${YOUR_DOMAIN}/ads/cancel?job_id=${job_id}`,
+    });
+    await pool.query(
+      'INSERT INTO transactions (job_id, session_id, created_at, payment_amount) values ($1, $2, $3, $4)',
+      [job_id, session.id, new Date(), budget]
+    );
+    res.json({ url: session.url });
+  } catch (error) {
+    console.error('Error creating checkout session:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
-adsRouter.get("/success/jobs/:job_id", async (req, res) => {
+adsRouter.get('/success/jobs/:job_id', async (req, res) => {
   const { job_id } = req.params;
   try {
     const sessionData = await pool.query(
@@ -78,7 +83,7 @@ adsRouter.get("/success/jobs/:job_id", async (req, res) => {
                                      WHERE job_id = ${job_id}`;
 
     await pool.query(updateTransectionQuery);
-    if (payment_status === "paid" && !accumulation_added) {
+    if (payment_status === 'paid' && !accumulation_added) {
       const jobData = await pool.query(jobAcmData);
       await pool.query(updateAccumulationQuery, [
         jobData.rows[0].job_payment_accumulation,
@@ -87,22 +92,25 @@ adsRouter.get("/success/jobs/:job_id", async (req, res) => {
         `UPDATE transactions SET accumulation_added = true WHERE transaction_id = ${transaction_id}`
       );
       return res.status(200).json({
-        message: "Update Payment Accumulation Success",
-        data: { email: session.email, amount: session.amount_total / 100 },
+        message: 'Update Payment Accumulation Success',
+        data: {
+          email: session.customer_details.email,
+          amount: session.amount_total / 100,
+        },
       });
     }
-    return res
-      .status(200)
-      .json({
-        message: "Update transaction success",
-        data: { email: session.email, amount: session.amount_total / 100 },
-      });
+    return res.status(200).json({
+      message: 'Update transaction success',
+      data: {
+        email: session.customer_details.email,
+        amount: session.amount_total / 100,
+      },
+    });
   } catch (error) {
     console.error(error);
     return res
       .status(500)
-      .json({ message: "An error occurred while update data" });
+      .json({ message: 'An error occurred while update data' });
   }
 });
 export default adsRouter;
-

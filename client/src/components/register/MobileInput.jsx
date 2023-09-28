e180934ec81f56a3dc1517ae2769ee1672bc7103
mrakkapol <mr.akkapol@gmail.com>
2023-09-28 13:14:45 +0700

    fix:fix query of search when showing higest paid jobs

---
+++ client/src/components/register/MobileInput.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import { Box, Heading, ChakraProvider, CSSReset, Text } from "@chakra-ui/react";
+import { useGlobalContext } from "../../contexts/registerContext.jsx";
+import PhoneNumberInput from "./mobileInput/PhoneNumInput";
+import { COUNTRIES } from "./mobileInput/countries";
+
+function MobileInput() {
+  const countryOptions = COUNTRIES.map(({ name, iso }) => ({
+    label: name,
+    value: iso
+  }));
+  const {
+    phone,
+    setPhone
+  } = useGlobalContext();
+
+  return (
+    <ChakraProvider>
+      <CSSReset />
+      <Box>
+        <Text pb="8px">Value: {phone}</Text>
+        <PhoneNumberInput
+          mr={8}
+          value={phone}
+          options={countryOptions}
+          placeholder="Enter phone number"
+          onChange={phone => setPhone(phone)}
+        />
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default MobileInput
